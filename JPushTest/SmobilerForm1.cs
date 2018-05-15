762c4ecf8a2850a46ec35e3dd9b42898762ca1bc
peter <peter.pan@searching-info.com>
2018-05-15 15:19:16 +0800

    update

---
+++ JPushTest/SmobilerForm1.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Smobiler.Core;
+using Smobiler.Core.Controls;
+using Smobiler.Utility.JPush;
+
+namespace JPushTest
+{
+    partial class SmobilerForm1 : Smobiler.Core.Controls.MobileForm
+    {
+        /// <summary>
+        /// 极光Key
+        /// </summary>
+        private static string appKey = "用户填写";
+        private static string appSecret = "用户填写";
+        PushClient client = new PushClient(appKey, appSecret);
+
+        private void button1_Press(object sender, EventArgs e)
+        {
+            this.Client.Push.SetAlias(this.textBox1.Text, (obj, args) =>
+            {
+                if (args.isError == true)
+                    this.Toast(args.error);
+                else
+                    this.Toast("Success");
+            });
+        }
+
+        private void button2_Press(object sender, EventArgs e)
+        {
+            client.Push(textBox2.Text, textBox1.Text);
+        }
+
+        private void button3_Press(object sender, EventArgs e)
+        {
+            client.PushAll(textBox2.Text, PushPlatform.android);
+        }
+
+        private void button4_Press(object sender, EventArgs e)
+        {
+            client.PushAlert(textBox2.Text, textBox1.Text, textBox1.Text);
+        }
+
+        private void button5_Press(object sender, EventArgs e)
+        {
+            client.PushAlertAll(textBox2.Text, textBox1.Text, PushPlatform.android);
+        }
+
+        private void button6_Press(object sender, EventArgs e)
+        {
+            client.PushURL(textBox2.Text, "https://www.baidu.com/", textBox1.Text);
+        }
+
+        private void button7_Press(object sender, EventArgs e)
+        {
+            client.PushURLAll(textBox2.Text, "https://www.baidu.com/", PushPlatform.android);
+        }
+
+        private void button8_Press(object sender, EventArgs e)
+        {
+            client.PushClientCallBack(textBox2.Text, textBox2.Text, textBox1.Text);
+        }
+
+        private void button9_Press(object sender, EventArgs e)
+        {
+            client.PushClientCallBackAll(textBox2.Text, textBox2.Text, PushPlatform.android);
+        }
+
+        private void button10_Press(object sender, EventArgs e)
+        {
+            client.PushBigText(textBox2.Text, textBox2.Text, textBox1.Text, textBox1.Text);
+        }
+
+        private void button11_Press(object sender, EventArgs e)
+        {
+            client.PushBigTextAll(textBox2.Text, textBox2.Text, textBox1.Text,PushPlatform.all);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# demo-jpush
-smobiler 极光推送插件demo
+## JPush插件
+#### 插件说明：
+集成极光推送
+
+#### 方法：
+|方法|方法说明|
+|:-|:-| 
+|Push|向指定别名推送消息（应用弹出）|
+|PushAll  |向全部设备推送消息（应用弹出）|
+|PushBigText  |向指定别名推送大文本|
+|PushBigTextAll  |向全部设备推送大文本|
+|PushAlert  |向指定别名推送通知（抬头栏）|
+|PushAlertAll  |向全部设备推送通知（抬头栏）|
+|PushURL  |向指定别名推送点击跳转URL链接的消息|
+|PushURLAll  |向全部设备推送点击跳转URL链接的消息|
+|PushClientCallBack|向指定别名推送点击触发客户端回调的消息|
+|PushClientCallBackAll|向全部设备推送点击触发客户端回调的消息|
+|SetMessage|设置Message|
+
+设置别名 this.Client.Push.SetAlias()
+
+### 源代码地址
+
